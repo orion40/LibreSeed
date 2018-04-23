b2b662a891713cf667542870eb1cfd341d2bc3aa
orion40 <qolizion@protonmail.com>
2018-04-23 13:44:14 +0200

    Add update function

--- src/Seed.cpp
+++ src/Seed.cpp
@@ -53,6 +53,7 @@ bool Seed::save_to_db(sqlite3* db){
     sqlite3_stmt* read_stmt;
     if (get_id() == -1) {
         // non sauvegarde en BDD, il faut la créé
+        // et récupéré l'ID associé
         std::string insert_stmt_string = "INSERT INTO seed (seed_name, seed_binomial_name, seed_description) VALUES (?, ?, ?);";
         change_query_result = sqlite3_prepare_v2(db, insert_stmt_string.c_str(), -1, &edition_stmt, NULL);
         switch (change_query_result){
@@ -63,8 +64,9 @@ bool Seed::save_to_db(sqlite3* db){
                 switch (sqlite3_step(edition_stmt)){
                     case SQLITE_DONE:
                         std::cout << "Successfully added Seed to database\n";
-                        set_modified(false);
                         sqlite3_finalize(edition_stmt);
+                        set_id(sqlite3_last_insert_rowid(db));
+                        set_modified(false);
                         break;
                     default:
                         std::cerr << "Error inserting into database.\n";
@@ -79,6 +81,31 @@ bool Seed::save_to_db(sqlite3* db){
     } else {
         if (is_modified()){
         // existe en BDD, et a été modifié, il faut update
+        // TODO: a TESTER !
+            std::string insert_stmt_string = "UPDATE seed SET seed_name = ?, seed_binomial_name = ?, seed_description = ? WHERE seed_id = ?;";
+            change_query_result = sqlite3_prepare_v2(db, insert_stmt_string.c_str(), -1, &edition_stmt, NULL);
+            switch (change_query_result){
+                case SQLITE_OK:
+                    sqlite3_bind_text(edition_stmt, 1, m_name.c_str(), -1, SQLITE_STATIC);
+                    sqlite3_bind_text(edition_stmt, 2, m_binomial_nomenclature.c_str(), -1, SQLITE_STATIC);
+                    sqlite3_bind_text(edition_stmt, 3, m_description.c_str(), -1, SQLITE_STATIC);
+                    sqlite3_bind_int(edition_stmt, 4, m_id);
+                    switch (sqlite3_step(edition_stmt)){
+                        case SQLITE_DONE:
+                            std::cout << "Successfully added Seed to database\n";
+                            set_modified(false);
+                            sqlite3_finalize(edition_stmt);
+                            break;
+                        default:
+                            std::cerr << "Error inserting into database.\n";
+                            break;
+                    }
+                    return true;
+                    break;
+                default:
+                    std::cerr << "Error preparing sql statement: " << sqlite3_errmsg(db) << std::endl;
+                    return false;
+            }
 
         }
     }
