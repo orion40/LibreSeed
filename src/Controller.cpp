5f982fb29e5ac45ba0e8428561ffe8b96f7da599
orion40 <qolizion@protonmail.com>
2018-04-27 12:27:55 +0200

    Properly return values

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -9,13 +9,13 @@ Controller::~Controller(){
 }
 
 bool Controller::open_db(){
-    m_model->open_db();
+    return m_model->open_db();
 }
 
 bool Controller::load_db(){
-    m_model->load_db();
+    return m_model->load_db();
 }
 
 bool Controller::create_db(){
-    m_model->create_db();
+    return m_model->create_db();
 }
